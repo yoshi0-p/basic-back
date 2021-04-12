e519e9d70f7d3ed641782a7b495b60f2becf8fdc
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-04-12 19:58:59 +0900

    message

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todos;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Todos::all();
+        return response()->json([
+            'message' => 'OK',
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Todos;
+        $item->event = $request->event;
+        $item->save();
+        return response()->json([
+            'message' => 'Created successfully',
+            'data' => $item
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todos $todos)
+    {
+        $item = Todos::where('id', $todos->id)->first();
+        $item->event = $request->event;
+        $item->save();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todos $todos)
+    {
+        $item = Todos::where('id', $todos->id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => $item,
+            ], 404);
+        }
+    }
+}
